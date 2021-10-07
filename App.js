c8a8ed6fca1d98ee667eabe112581c6ab9f53005
= <connorruff99@gmail.com>
2021-10-07 18:33:33 -0400

    set up

--- App.js
+++ App.js
@@ -4,8 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}> 
+
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Top Nav Bar</Text>
+      </View>
+
+      <View style={styles.body}>
+        <Text>Body</Text>
+      </View>
+
+      <View style={styles.bottomBar}>
+        <Text>Bottom Nav Bar</Text>
+      </View>
+      
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +26,34 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 3,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+
+  header: {
+    backgroundColor: '#DF00FF',
+    padding: 20,
+    width: '100%',
+    paddingTop: '15%',
+  
+
+  },
+
+  body: {
+    height: '75%'
+  },
+
+  bottomBar: {
+    backgroundColor: '#DF00FF',
+    padding: 10,
+    width: '100%',
+    height: '25%'
+  }
+
+
+
+
+
 });
