ef842069d4bfb909faf562c58db192d286a01e34
jnguyen38 <jnguyen8831@gmail.com>
2022-03-24 13:09:22 -0400

    Login Screen Keyboard User Functionality (DismissView) and Design Modifications

--- components/Login.js
+++ components/Login.js
@@ -8,16 +8,29 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
+  Keyboard,
+  Animated,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
 } from "react-native";
+
+// Dismiss the Keyboard by touching anywhere on the screen using
+// the TouchableWithoutFeedback and Keyboard package to create a handler component
+const DismissKeyboard = ({ children }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+)
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  //id, email, password, name, position, auth_token
+  // id, email, password, name, position, auth_token
   var temp = new User(1, "Test@nd.edu", "password", "user", "dev", 1231221312);
+
   function forgotPassword() {
     navigation.navigate("ForgotPassword");
-    //need email system to authenticate user to change password
+    // need email system to authenticate user to change password
   }
 
   function createAccount() {
@@ -25,11 +38,9 @@ export default function Login({ navigation }) {
   }
 
   function validateForm() {
-    //for testing uncomment below
-    //so you do not have to input something for login
+    // for testing uncomment below so you do not have to input something for login
     navigation.navigate("MainTabNavigator");
-    //
-
+    {/*
     if (email.length > 0 && password.length > 0 && (email == temp.email && password == temp.password)) {
         setEmail("");
         setPassword("");
@@ -37,42 +48,58 @@ export default function Login({ navigation }) {
       } else {
         alert("Incorrect email or password");
       }
+    */}
   }
+
+
+
+  // Comments inside of the render block need to be in JSX multiline format: {/*COMMENT*/}
+
   return (
-    <View style={styles.container}>
+    <DismissKeyboard>
+    <KeyboardAvoidingView style={styles.container} behavior="padding" >
+
+      {/* Goodwill Image*/}
       <Image style={styles.image} source={require("../assets/logo2.png")}/>
       <StatusBar style="auto" />
+
+      {/*Email input field*/}
       <View style={styles.inputView}>
         <TextInput
           style={styles.TextInput}
-          placeholder="Email..."
-          placeholderTextColor="white"    
+          placeholder="Username or email"
+          placeholderTextColor="#7a7a7a"
           onChangeText={(email) => setEmail(email)}
         />
       </View>
 
+      {/*Password input field*/}
       <View style={styles.inputView}>
         <TextInput
           style={styles.TextInput}
-          placeholder="Password..."
-          placeholderTextColor="white"
+          placeholder="Password"
+          placeholderTextColor="#7a7a7a"
           secureTextEntry={true}
           onChangeText={(password) => setPassword(password)}
         />
       </View>
 
+      {/*Forgot Password Button*/}
       <TouchableOpacity onPress={() => forgotPassword()}>
-        <Text style={styles.forgot_button}>Forgot Password?</Text>
+        <Text style={styles.forgot_text}>Forgot Password?</Text>
       </TouchableOpacity>
 
+      {/*Create Account Button*/}
       <TouchableOpacity onPress={() => createAccount()}>
-        <Text style={styles.forgot_button}>Create Account?</Text>
+        <Text style={styles.forgot_text}>Create Account</Text>
       </TouchableOpacity>
 
+      {/*Login Button*/}
       <TouchableOpacity style={styles.loginBtn} onPress={() => validateForm()}>
-        <Text style={styles.loginText}>LOGIN</Text>
+        <Text style={styles.loginText}>Log In</Text>
       </TouchableOpacity>
-    </View>
+    </KeyboardAvoidingView>
+    </DismissKeyboard>
   );
 }
 
@@ -91,37 +118,47 @@ const styles = StyleSheet.create({
   },
 
   inputView: {
-    backgroundColor: "blue",
-    borderRadius: 30,
+    backgroundColor: '#f2f2f2',
+    borderColor: '#e0e0e0',
+    borderRadius: 8,
+    borderWidth: 1,
     width: "70%",
     height: 45,
     marginBottom: 20,
-
     alignItems: "center",
   },
 
   TextInput: {
     height: 50,
     flex: 1,
-    padding: 10,
+    borderRadius: 8,
+    borderColor: '#e0e0e0',
+    paddingLeft: 12,
     marginLeft: 0,
     width: "100%",
-    textAlign: "center",
-    color: "white",
+    textAlign: "left",
+    color: "black",
   },
 
-  forgot_button: {
+  forgot_text: {
     height: 30,
-    marginBottom: 30,
+    marginBottom: 0,
+    color: "#01529c",
+    // fontWeight: "bold",
   },
 
   loginBtn: {
     width: "80%",
-    borderRadius: 25,
+    borderRadius: 8,
     height: 50,
     alignItems: "center",
     justifyContent: "center",
     marginTop: 40,
-    backgroundColor: "#009dff",
+    backgroundColor: "#01529c",
   },
+
+  loginText: {
+    color: "white",
+    fontWeight: "bold",
+  }
 });
