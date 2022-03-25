3fa74e4bfabfe3e5672e877a2fa5545e9b35e0da
jnguyen38 <jnguyen8831@gmail.com>
2022-03-24 22:59:26 -0400

    Media Updates and StyleSheet Centralization

--- assets/StyleSheet.js
+++ assets/StyleSheet.js
@@ -1,7 +1,7 @@
 import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: .89,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
@@ -14,22 +14,26 @@ export const styles = StyleSheet.create({
   },
 
   inputView: {
-    backgroundColor: "blue",
-    borderRadius: 30,
+    backgroundColor: '#f2f2f2',
+    borderColor: '#e0e0e0',
+    borderRadius: 8,
+    borderWidth: 1,
     width: "70%",
     height: 45,
-    marginBottom: 20,
+    marginBottom: 15,
     alignItems: "center",
   },
 
   TextInput: {
     height: 50,
     flex: 1,
-    padding: 10,
+    borderRadius: 8,
+    borderColor: '#e0e0e0',
+    paddingLeft: 12,
     marginLeft: 0,
     width: "100%",
-    textAlign: "center",
-    color: "white",
+    textAlign: "left",
+    color: "black",
   },
 
   forgot_button: {
@@ -37,18 +41,58 @@ export const styles = StyleSheet.create({
     marginBottom: 30,
   },
 
-  loginBtn: {
-    width: "80%",
-    borderRadius: 25,
+  mainBtn: {
+    width: "70%",
+    borderRadius: 8,
     height: 50,
     alignItems: "center",
     justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#009dff",
+    marginTop: 30,
+    backgroundColor: "#01529c",
   },
+
+  mainBtnText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+
   headerText: {
     fontSize: 20,
+    fontWeight: "bold",
     color: "black",
     padding: 20,
   },
+
+  backBtn: {
+    alignSelf: "flex-end",
+    flex: 1,
+    marginHorizontal: "5%",
+    marginBottom: "3%",
+    zIndex: 3,
+  },
+
+  backBtnText: {
+    fontSize: 35,
+    fontWeight: "bold",
+    color: "white",
+  },
+
+  headerBar: {
+    height: "11%",
+    alignItems: "flex-start",
+    justifyContent: "space-around",
+    backgroundColor: "#01529c",
+    zIndex: 1,
+    flexDirection: "row"
+  },
+
+  headerBarImg: {
+    zIndex: 2,
+    width: 150,
+    height: 150,
+    marginTop: "8%",
+    resizeMode: 'contain',
+    alignSelf: "center",
+  }
+
 });
