46dfd2da206639cd8c690ad6f59c24046c4d2939
jnguyen38 <jnguyen8831@gmail.com>
2022-03-24 22:56:50 -0400

    Login Page User Functionality and Graphic Design

--- components/Login.js
+++ components/Login.js
@@ -13,6 +13,7 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
 } from "react-native";
+// import { ReactModules } from "../assets/index.js";
 
 // Dismiss the Keyboard by touching anywhere on the screen using
 // the TouchableWithoutFeedback and Keyboard package to create a handler component
@@ -29,16 +30,20 @@ export default function Login({ navigation }) {
   var temp = new User(1, "Test@nd.edu", "password", "user", "dev", 1231221312);
 
   function forgotPassword() {
+    Keyboard.dismiss();
     navigation.navigate("ForgotPassword");
     // need email system to authenticate user to change password
   }
 
   function createAccount() {
+    Keyboard.dismiss();
     navigation.navigate("CreateAccount");
+
   }
 
   function validateForm() {
     // for testing uncomment below so you do not have to input something for login
+    Keyboard.dismiss();
     navigation.navigate("MainTabNavigator");
     {/*
     if (email.length > 0 && password.length > 0 && (email == temp.email && password == temp.password)) {
@@ -56,50 +61,60 @@ export default function Login({ navigation }) {
   // Comments inside of the render block need to be in JSX multiline format: {/*COMMENT*/}
 
   return (
-    <DismissKeyboard>
-    <KeyboardAvoidingView style={styles.container} behavior="padding" >
-
-      {/* Goodwill Image*/}
-      <Image style={styles.image} source={require("../assets/logo2.png")}/>
-      <StatusBar style="auto" />
-
-      {/*Email input field*/}
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Username or email"
-          placeholderTextColor="#7a7a7a"
-          onChangeText={(email) => setEmail(email)}
-        />
+    <React.Fragment>
+      <View style={styles.headerBar}>
+        <Image style={styles.headerBarImg} source={require("../assets/goodwill-text-transparent.png")}/>
       </View>
 
-      {/*Password input field*/}
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password"
-          placeholderTextColor="#7a7a7a"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </View>
+      <DismissKeyboard>
 
-      {/*Forgot Password Button*/}
-      <TouchableOpacity onPress={() => forgotPassword()}>
-        <Text style={styles.forgot_text}>Forgot Password?</Text>
-      </TouchableOpacity>
+        <KeyboardAvoidingView style={styles.container} behavior="padding" >
 
-      {/*Create Account Button*/}
-      <TouchableOpacity onPress={() => createAccount()}>
-        <Text style={styles.forgot_text}>Create Account</Text>
-      </TouchableOpacity>
+          {/* Goodwill Image*/}
+          <Image style={styles.image} source={require("../assets/logo-img.png")}/>
 
-      {/*Login Button*/}
-      <TouchableOpacity style={styles.loginBtn} onPress={() => validateForm()}>
-        <Text style={styles.loginText}>Log In</Text>
-      </TouchableOpacity>
-    </KeyboardAvoidingView>
-    </DismissKeyboard>
+          <View>
+            <Text style={styles.headerText}>Sign In</Text>
+          </View>
+
+          {/*Email input field*/}
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Username or email"
+              placeholderTextColor="#7a7a7a"
+              onChangeText={(email) => setEmail(email)}
+            />
+          </View>
+
+          {/*Password input field*/}
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Password"
+              placeholderTextColor="#7a7a7a"
+              secureTextEntry={true}
+              onChangeText={(password) => setPassword(password)}
+            />
+          </View>
+
+          {/*Forgot Password Button*/}
+          <TouchableOpacity onPress={() => forgotPassword()}>
+            <Text style={styles.forgot_text}>Forgot Password?</Text>
+          </TouchableOpacity>
+
+          {/*Create Account Button*/}
+          <TouchableOpacity onPress={() => createAccount()}>
+            <Text style={styles.forgot_text}>Create Account</Text>
+          </TouchableOpacity>
+
+          {/*Login Button*/}
+          <TouchableOpacity style={styles.loginBtn} onPress={() => validateForm()}>
+            <Text style={styles.loginText}>Log In</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </DismissKeyboard>
+    </React.Fragment>
   );
 }
 
@@ -112,9 +127,10 @@ const styles = StyleSheet.create({
   },
 
   image: {
-    marginBottom: 40,
-    width: 250,
-    height: 300,
+    marginBottom: 0,
+    marginTop: 0,
+    flex: .65,
+    resizeMode: "contain",
   },
 
   inputView: {
@@ -124,12 +140,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     width: "70%",
     height: 45,
-    marginBottom: 20,
+    marginBottom: 15,
     alignItems: "center",
   },
 
   TextInput: {
-    height: 50,
     flex: 1,
     borderRadius: 8,
     borderColor: '#e0e0e0',
@@ -148,17 +163,46 @@ const styles = StyleSheet.create({
   },
 
   loginBtn: {
-    width: "80%",
+    width: "70%",
     borderRadius: 8,
     height: 50,
     alignItems: "center",
     justifyContent: "center",
-    marginTop: 40,
+    marginTop: 30,
     backgroundColor: "#01529c",
   },
 
   loginText: {
     color: "white",
     fontWeight: "bold",
-  }
+  },
+
+  headerBar: {
+    height: "11%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#01529c",
+    zIndex: 1
+  },
+
+  headerBarText: {
+    zIndex: 2
+  },
+
+  headerBarImg: {
+    zIndex: 2,
+    flex: 1,
+    width: 150,
+    height: 150,
+    marginTop: "8%",
+    resizeMode: 'contain'
+  },
+
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "black",
+    padding: 20,
+  },
+
 });
