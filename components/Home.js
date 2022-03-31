fd789f0be5990185ad2666bf011642a5487eaf21
jnguyen38 <jnguyen8831@gmail.com>
2022-03-31 17:22:25 -0400

    Initial Map updates and some functionality

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import {
   StyleSheet,
+  ScrollView,
   Text,
   View,
   TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
   FlatList,
+  Button,
 } from "react-native";
 import { Link } from "@react-navigation/native";
 import TopBar from "../components/TopBar";
+import MapView from 'react-native-maps';
+import { styles } from "../assets/StyleSheet";
 
 export default function Home({ navigation }) {
   const [pages, setPages] = useState([
@@ -16,34 +22,40 @@ export default function Home({ navigation }) {
     { name: "Resources", key: "3" },
   ]);
 
+  function toMaps() {
+    navigation.navigate("Map");
+  }
+
+
   return (
-    <View style={styles.container}>
-      <TopBar navigation={navigation}></TopBar>
-      <FlatList
-        keyExtractor={(item) => item.key}
-        data={pages}
-        renderItem={({ item }) => (
-          <TouchableOpacity>
-            <Link style={styles.button} to={{ screen: item.name }}>
-              <Text>{item.name}</Text>
-            </Link>
-          </TouchableOpacity>
-        )}
-      />
-    </View>
+    <React.Fragment>
+      <View style={styles.containerHome}>
+        <TopBar navigation={navigation}></TopBar>
+        <TouchableOpacity style={styles.mapContainer}
+          activeOpacity='.7'>
+          <MapView
+            style={styles.mapView}
+            onPress={() => toMaps()}
+            showsUserLocation
+            initialRegion={{
+              latitude: 41.6764,
+              longitude: -86.2520,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421}}
+          />
+        </TouchableOpacity>
+        <FlatList
+          keyExtractor={(item) => item.key}
+          data={pages}
+          renderItem={({ item }) => (
+            <TouchableOpacity>
+              <Link style={styles.button} to={{ screen: item.name }}>
+                <Text>{item.name}</Text>
+              </Link>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    </React.Fragment>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white", 
-    flex: 1
-  },
-  button: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "#ddd",
-    fontSize: 24,
-    marginHorizontal: 10,
-  },
-});
--- components/Map.js
+++ components/Map.js
@@ -1,10 +1,32 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { userState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
 
-export default function Map({navigator}) {
+} from "react-native";
+import {styles} from "../assets/StyleSheet";
+import MapView from 'react-native-maps';
+
+
+export default function Map({ navigation }) {
+
+  function backToHome() {
+    navigation.navigate("Home")
+  }
+
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+    <React.Fragment>
+        <MapView
+          style={styles.mainMapView}
+          showsUserLocation
+          initialRegion={{
+            latitude: 41.6764,
+            longitude: -86.2520,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421}}
+        />
+    </React.Fragment>
   );
 }
