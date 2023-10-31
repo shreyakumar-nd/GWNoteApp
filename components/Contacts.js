9481416ca552d75ff2758b0ac52b7cdd0af7a37b
gggabe00 <gsheikh@nd.edu>
2023-10-31 16:35:46 -0400

    Contacts Page and Individual Contact Page routing

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,228 +1,107 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SectionList,
-  TouchableOpacity
-} from "react-native";
-import { Link } from "@react-navigation/native";
-import { ScrollView } from "react-native-gesture-handler";
-import { Contact } from "./Classes/Contact";
-import ContactsTopBar from "./ContactsTopBar";
-import IndividualContact from "./IndividualContact";
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, SectionList } from 'react-native';
+import axios from 'axios';
 
-export default function Contacts({ navigation: { navigate } }) {
-  const [contacts, setContants] = useState([ 
-    {
-      index: 0,
-      name: "Nicole Mellert"
-    },
-    {
-      index: 1,
-      name: "Jesse Hooper"
-    },
-    {
-      index: 2,
-      name: "Reid Zavala"
-    },
-    {
-      index: 3,
-      name: "Milo Alvarado"
-    },
-    {
-      index: 4,
-      name: "Tiara Hinton"
-    },
-    {
-      index: 5,
-      name: "Zion Bowers"
-    },
-    {
-      index: 6,
-      name: "Rebekah Case"
-    },
-    {
-      index: 7,
-      name: "Landin Landry"
-    },
-    {
-      index: 8,
-      name: "Nevaeh Hicks"
-    },
-    {
-      index: 9,
-      name: "Shyanne Kerr"
-    },
-    {
-      index: 10,
-      name: "Yaretzi Bryan"
-    },
-    {
-      index: 11,
-      name: "Ramos Bauer"
-    },
-    {
-      index: 12,
-      name: "Humberto Lester"
-    },
-    {
-      index: 13,
-      name: "Sienna Knight"
-    },
-    {
-      index: 14,
-      name: "Allen Jensen"
-    },
-    {
-      index: 15,
-      name: "Shaniya Wood"
-    },
-    {
-      index: 16,
-      name: "Brielle Pacheco"
-    },
-    {
-      index: 17,
-      name: "Mira Fry"
-    },
-    {
-      index: 18,
-      name: "Rachel Beck"
-    },
-    {
-      index: 19,
-      name: "Lina Bowers"
-    },
-    {
-      index: 20,
-      name: "Vance Nichols"
-    },
-    {
-      index: 21,
-      name: "Elian Herring"
-    },
-    {
-      index: 22,
-      name: "Kristin Estes"
-    },
-    {
-      index: 23,
-      name: "Trevor Beasley"
-    },
-    {
-      index: 24,
-      name: "Weston Haynes"
-    },
-    {
-      index: 25,
-      name: "Dayton Ferguson"
-    },
-    {
-      index: 26,
-      name: "Gemma Harper"
-    },
-    {
-      index: 27,
-      name: "Jordan Frye"
-    },
-    {
-      index: 28,
-      name: "Charlie Tyler"
-    },
-    {
-      index: 29,
-      name: "Maeve Graves"
-    },
-    {
-      index: 30,
-      name: "Ronan Mills"
-    },
-    {
-      index: 31,
-      name: "Donovan Riddle"
-    },
-    {
-      index: 32,
-      name: "Matteo Zuniga"
-    },
-    {
-      index: 33,
-      name: "Denise Booth"
-    },
-    {
-      index: 34,
-      name: "Audrey Mill"
-    },
-    {
-      index: 35,
-      name: "Austin Humphrey"
-    },
-    {
-      index: 36,
-      name: "Amy Hills"
-    },
-    {
-      index: 37,
-      name: "Nicole Mellert"
-    }
+const baseApiUrl = 'http://localhost:3000/workers';
 
-  ])
+const Contacts = ({ navigation }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [workers, setWorkers] = useState([]);
+  const [filteredWorkers, setFilteredWorkers] = useState([]);
+  const [error, setError] = useState(null);
 
-
-  const getData = () => {
-    let contactsArr = [];
-    let aCode = "A".charCodeAt(0);
-    for (let i = 0; i < 26; i++) {
-      let currChar = String.fromCharCode(aCode + i);
-      let obj = {
-        title: currChar
-      };
-
-      let currContacts = contacts.filter(item => {
-        return item.name[0].toUpperCase() === currChar;
+  useEffect(() => {
+    axios.get(`${baseApiUrl}`)
+      .then(response => {
+        const workersData = response.data;
+        setWorkers(workersData);
+        setFilteredWorkers(workersData); // Initially, no filter applied
+      })
+      .catch(error => {
+        setError(error);
       });
-      if (currContacts.length > 0) {
-        currContacts.sort((a, b) => a.name.localeCompare(b.name));
-        obj.data = currContacts;
-        contactsArr.push(obj);
+  }, []);
+
+  useEffect(() => {
+    if (searchTerm === "") {
+      setFilteredWorkers(workers);
+    } else {
+      const filteredData = workers.filter(worker =>
+        worker.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        worker.lastName.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredWorkers(filteredData);
+    }
+  }, [searchTerm, workers]);
+
+  const getSortedData = () => {
+    let sortedData = [];
+    const grouped = filteredWorkers.reduce((groups, worker) => {
+      const firstLetter = worker.firstName[0].toUpperCase();
+      if (!groups[firstLetter]) {
+        groups[firstLetter] = [];
       }
+      groups[firstLetter].push(worker);
+      return groups;
+    }, {});
+
+    for (let [letter, workersGroup] of Object.entries(grouped)) {
+      sortedData.push({
+        title: letter,
+        data: workersGroup.sort((a, b) => a.firstName.localeCompare(b.firstName)),
+      });
     }
 
-    return contactsArr;
+    return sortedData;
   };
 
-  return(
+  return (
     <View style={styles.container}>
+      <Text style={styles.h1}>Worker List</Text>
 
-      <SectionList
-        sections={getData()}
-        renderItem={({ item }) => (
-          <View>
-            <TouchableOpacity style={styles.row} onPress={() => navigate('IndividualContact')}>
-              <Text>{item.name}</Text>
-            </TouchableOpacity>
-     
-            <View style={{width: '90%', alignSelf: 'center', height: 1, backgroundColor: '#efefef'}}/> 
-          </View>
-        )}
-        renderSectionHeader={({ section }) => (
-          <View style={styles.sectionHeader}>
-            <Text>{section.title}</Text>
-          </View>
-        )}
-        keyExtractor={item => item.index}
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search workers..."
+        value={searchTerm}
+        onChangeText={setSearchTerm} // This will trigger the useEffect for filtering
       />
+
+      {error ? (
+        <Text>Error: {error.message}</Text>
+      ) : (
+        <SectionList
+          sections={getSortedData()}
+          renderItem={({ item }) => (
+            <View>
+              <TouchableOpacity
+                style={styles.row}
+                onPress={() => {
+                  navigation.navigate('IndividualContact', { contactName: `${item.firstName} ${item.lastName}` });
+                }}
+              >
+                <Text>{`${item.firstName} ${item.lastName}`}</Text>
+              </TouchableOpacity>
+
+              <View style={styles.separator} />
+            </View>
+          )}
+          renderSectionHeader={({ section }) => (
+            <View style={styles.sectionHeader}>
+              <Text>{section.title}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => String(item.workerID)}
+        />
+      )}
     </View>
-    
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: "#fff",
-    flexDirection: "row",
+    flexDirection: "column",
     alignSelf: "stretch",
   },
   row: {
@@ -233,5 +112,24 @@ const styles = StyleSheet.create({
     backgroundColor: "#efefef",
     paddingHorizontal: 20,
     paddingVertical: 10, 
-  }
+  },
+  h1: {
+    fontSize: 24, 
+    fontWeight: "bold", 
+    textAlign: "center", 
+    margin: 20, 
+  },
+  searchInput: {
+    height: 40,
+    width: "80%",
+    borderRadius: 9,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    alignSelf: "center",
+    marginBottom: 20,
+  },
 });
+
+export default Contacts;
--- components/IndividualContact.js
+++ components/IndividualContact.js
@@ -8,10 +8,13 @@ import {
 } from "react-native";
 
 export default function IndividualContact({ navigation, route }) {
+    // Extract contactName from route params
+    const { contactName } = route.params;
 
     return (
         <View style={styles.container}>
-            
+            {/* Display the contact's name */}
+            <Text style={styles.nameText}>{contactName}</Text>
         </View>
     );
 }
@@ -21,6 +24,11 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "blue",
         alignSelf: "stretch",
+        justifyContent: "center",  // Center content vertically
+        alignItems: "center"       // Center content horizontally
+    },
+    nameText: {
+        fontSize: 24,
+        color: "white"
     }
-    }
-);
+});
